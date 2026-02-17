b864f66009086958fea67eb4a1e88dcc1e7afeac
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2026-02-17 14:07:22 +0100

    Add data

--- download.sh
+++ download.sh
@@ -1,2 +1,1 @@
-wget -O data.zip "https://cloud.geo.tuwien.ac.at/s/nRritdWT8CCQKHq/download" && unzip data.zip
-wget -O extra_era5_europe.zip "https://cloud.geo.tuwien.ac.at/s/EX4ADiy5jcDKSej/download" && unzip extra_era5_europe.zip
+wget -O extra_era5land_europe.zip "https://cloud.geo.tuwien.ac.at/s/EX4ADiy5jcDKSej/download" && unzip extra_era5land_europe.zip && rm extra_era5land_europe.zip
